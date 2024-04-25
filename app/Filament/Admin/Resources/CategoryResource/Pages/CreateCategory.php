a513a2af273edd020965a069f38d80f2b1f0357a
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-25 13:56:11 +0700

    Update CreateCategory after move CategoryResource file on CategoryResource folder
    Create Nofitications after create Category Service, Create getRedictUrl function

--- app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Admin/Resources/CategoryResource/Pages/CreateCategory.php
@@ -2,11 +2,31 @@
 
 namespace App\Filament\Admin\Resources\CategoryResource\Pages;
 
-use App\Filament\Admin\Resources\CategoryResource;
-use Filament\Actions;
+use App\Filament\Admin\Resources\CategoryResource\CategoryResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected  function getRedirectUrl(): string
+    {
+        return CategoryResource::getUrl('index');
+    }
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Tạo loại dịch vụ thành công')
+            ->success()
+            ->duration(3000)
+            ->send();
+
+    }
+
+    protected function afterCreate():void
+    {
+        logger('Sau khi tạo');
+        logger($this->record);
+    }
 }
