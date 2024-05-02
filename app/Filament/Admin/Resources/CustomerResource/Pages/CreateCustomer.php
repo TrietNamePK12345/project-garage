bd5f64cebf2ce0f38002a3d3b66e9e8722e5cd35
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-02 20:36:33 +0700

    Update CreateCustomer
    Add getRedictUrl method and getCreateNotification Method

--- app/Filament/Admin/Resources/CustomerResource/Pages/CreateCustomer.php
+++ app/Filament/Admin/Resources/CustomerResource/Pages/CreateCustomer.php
@@ -3,9 +3,26 @@
 namespace App\Filament\Admin\Resources\CustomerResource\Pages;
 
 use App\Filament\Admin\Resources\CustomerResource\CustomerResource;
+use App\Filament\Admin\Resources\CustomerResource\Forms\CustomerForm;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return CustomerResource::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Khách hàng mới đã được tạo thành công!')
+            ->success()
+            ->duration(3000)
+            ->send();
+
+    }
 }
