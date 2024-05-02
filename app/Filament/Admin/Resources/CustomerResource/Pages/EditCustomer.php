51c872e3fe33d52ae801495e4748c3a2e8ff49af
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-02 21:50:51 +0700

    Update EditCustomer
    add
        protected function getSavedNotification(): ?Notification
        {
            return Notification::make()
                ->title('Đã cập nhật thành công')
                ->success()
                ->duration(3000)
                ->send();
        }

--- app/Filament/Admin/Resources/CustomerResource/Pages/EditCustomer.php
+++ app/Filament/Admin/Resources/CustomerResource/Pages/EditCustomer.php
@@ -4,6 +4,7 @@ namespace App\Filament\Admin\Resources\CustomerResource\Pages;
 
 use App\Filament\Admin\Resources\CustomerResource\CustomerResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCustomer extends EditRecord
@@ -16,5 +17,14 @@ class EditCustomer extends EditRecord
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Đã cập nhật thành công')
+            ->success()
+            ->duration(3000)
+            ->send();
     }
 }
