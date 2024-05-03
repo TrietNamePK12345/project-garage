4461cdfdc33099fea825ef2b5f976b5b597c0a37
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-03 13:30:20 +0700

    Update ListCustomer
    add getTabs method

--- app/Filament/Admin/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Admin/Resources/CustomerResource/Pages/ListCustomers.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Admin\Resources\CustomerResource\Pages;
 
 use App\Filament\Admin\Resources\CustomerResource\CustomerResource;
+use App\Models\Customer;
 use Filament\Actions;
+
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
 
 class ListCustomers extends ListRecords
 {
@@ -14,6 +18,22 @@ class ListCustomers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'Tất cả' => Tab::make(),
+            'Tuần này' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subWeek()))
+            ->badge(Customer::query()->where('created_at', '>=', now()->subWeek())->count()),
+            'Tháng này' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subMonth()))
+            ->badge(Customer::query()->where('created_at', '>=', now()->subMonth())->count()),
+            'Năm này' => Tab::make()
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subYear()))
+            ->badge(Customer::query()->where('created_at', '>=', now()->subYear())->count())
         ];
     }
 }
