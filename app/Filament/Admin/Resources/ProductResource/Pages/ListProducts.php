e5bd7349a9cb36213c6b063cf8b0ec746b86847a
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-26 13:04:37 +0700

    Update ListProducts.php
    add method getHeaderWidgets() and use ExposesTableToWidgets;

--- app/Filament/Admin/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Admin/Resources/ProductResource/Pages/ListProducts.php
@@ -3,17 +3,26 @@
 namespace App\Filament\Admin\Resources\ProductResource\Pages;
 
 use App\Filament\Admin\Resources\ProductResource\ProductResource;
+use App\Filament\Admin\Resources\ProductResource\Widgets\ProductWidget;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProducts extends ListRecords
 {
+    use ExposesTableToWidgets;
     protected static string $resource = ProductResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+          ProductWidget::class,
         ];
     }
 }
