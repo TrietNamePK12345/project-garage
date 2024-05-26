147fc92e18c41eb5d00ba2c3943bf49b287bab1e
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-26 11:01:35 +0700

    Create ProductAction
    Add method action and bulkActions

---
+++ app/Filament/Admin/Resources/ProductResource/Actions/ProductAction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ProductResource\Actions;
+
+use Filament\Support\Enums\ActionSize;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\DeleteAction;
+
+class ProductAction
+{
+    public static function action()
+    {
+        return [
+            ActionGroup::make([
+                ViewAction::make()->label('Xem chi tiết'),
+                EditAction::make()->label('Cập nhật'),
+                DeleteAction::make('Xóa')
+            ])
+                ->size(ActionSize::Small)
+                ->color('primary'),
+
+        ];
+    }
+
+    public static function bulkActions(): array
+    {
+        return [
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make()
+            ]),
+        ];
+    }
+}
