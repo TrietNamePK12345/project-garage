82ac1b05d6acb57ab24299062dd4a0da1c696bde
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-29 12:10:12 +0700

    Create EmployeeAction

---
+++ app/Filament/Admin/Resources/EmployeeResource/Actions/EmployeeAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Admin\Resources\EmployeeResource\Actions;
+
+use Filament\Support\Enums\ActionSize;
+use Filament\Tables;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\DeleteAction;
+
+class EmployeeAction
+{
+    public static function action()
+    {
+        return [
+            ActionGroup::make([
+                ViewAction::make()->label('Xem chi tiết'),
+                EditAction::make()->label('Cập nhật'),
+                DeleteAction::make('Xóa')
+            ])
+                ->size(ActionSize::Small)
+                ->color('primary')
+        ];
+    }
+
+    public static function bulkActions(): array
+    {
+        return [
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make()
+            ]),
+        ];
+    }
+}
