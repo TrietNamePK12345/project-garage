7746f7646cbaa6ff7c1797a35888e8522848bec2
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-05-29 17:53:41 +0700

    Update ListEmployees.php

--- app/Filament/Admin/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Admin/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -3,17 +3,27 @@
 namespace App\Filament\Admin\Resources\EmployeeResource\Pages;
 
 use App\Filament\Admin\Resources\EmployeeResource\EmployeeResource;
+use App\Filament\Admin\Resources\EmployeeResource\Widgets\EmployeeWidget;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListEmployees extends ListRecords
 {
+    use ExposesTableToWidgets;
     protected static string $resource = EmployeeResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            EmployeeWidget::class
         ];
     }
 }
